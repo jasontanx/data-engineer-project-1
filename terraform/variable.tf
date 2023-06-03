e7353d8fee7f0d2aecb8ca4cc6705a83643e148f
Jason Tan <116934441+jasontanx@users.noreply.github.com>
2023-06-03 14:08:02 +0800

    add variable terraform

---
+++ terraform/variable.tf
@@ -0,0 +1,10 @@
+variable "project_id" {
+  description = "The project id for this project"
+  type        = string
+  default     = "myfirstproject-364809"
+}
+
+variable "region" {
+  description = "specified region for this project"
+  default     = "asia-southeast1"
+}
